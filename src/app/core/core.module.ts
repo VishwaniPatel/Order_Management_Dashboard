2d61417ea6dafb5f4c5645b6fec2ead7a09cc4fb
Vishwani Patel <vishwani.patel@1rivet.local>
2023-02-24 17:21:03 +0530

    feat: header design

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
-
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, SidebarComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
